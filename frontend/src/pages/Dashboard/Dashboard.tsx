3e2b1f50650f1dae09a651edb317f4a5b48d8f3d
aleksandr.purtov <aleksandr.purtov@dxc.com>
2023-03-15 01:33:34 +0600

    feat: add query hello on Dashboard page

--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -1,18 +1,40 @@
+import { useQuery } from 'react-query';
 import { useAuthContext } from '../../hooks/useAuthContext';
 import { TypedLink } from '../../routers/components';
+import { ReactComponent as LoadingSVG } from '../../assets/loading.svg';
+import { api } from '../../api';
 
 export const Dashboard = () => {
     const { authContext } = useAuthContext();
+
+    const {
+        data: hello,
+        isSuccess,
+        isFetching,
+        refetch,
+    } = useQuery('hello', api.getHello, {
+        refetchOnWindowFocus: false,
+        enabled: false, // disable this query from automatically running
+    });
 
     return (
         <div>
             <h1>Dashboard</h1>
             <TypedLink to="/profile">To profile</TypedLink>
             <br />
+            <br />
+
+            {!isSuccess && <button onClick={() => refetch()}>Get Hello</button>}
+            <div className="App">{isFetching ? <LoadingSVG /> : hello?.greeting}</div>
+
+            <br />
+            <br />
+
             <button
                 onClick={() => {
                     authContext.signOut();
-                }}>
+                }}
+            >
                 Sign out
             </button>
         </div>
