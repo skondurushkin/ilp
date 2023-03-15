86cd05abd11c16e075e444d021236ab8c85d37d2
aleksandr.purtov <aleksandr.purtov@dxc.com>
2023-03-15 17:49:00 +0600

    fix: types for TypedNavigate

--- frontend/src/routers/components/TypedNavigate/TypedNavigate.tsx
+++ frontend/src/routers/components/TypedNavigate/TypedNavigate.tsx
@@ -1,13 +1,32 @@
-import { Navigate } from 'react-router-dom';
-import { RoutesPath } from '../../routes.types';
+import { createSearchParams, Navigate } from 'react-router-dom';
+import { PathParams, RoutesPath } from '../../routes.types';
 import type { NavigateProps } from 'react-router-dom';
+import { buildUrl } from '../../routes.utils';
 
-export const TypedNavigate = <P extends RoutesPath>({ to, ...props }: NavigatePropsAll<P>) => {
-    return <Navigate to={to} {...props} />;
+export const TypedNavigate = <P extends RoutesPath>({ to, params, search, ...props }: NavigatePropsAll<P>) => {
+    return (
+        <Navigate
+            to={{
+                pathname: buildUrl(to, params),
+                search: typeof search === 'object' ? createSearchParams(search).toString() : search,
+            }}
+            {...props}
+        />
+    );
 };
 
 interface TypedNavigateProps<P extends RoutesPath> {
     to: P;
+    params?: PathParams<P>;
+    search?: string;
 }
 
-export type NavigatePropsAll<P extends RoutesPath> = TypedNavigateProps<P> & NavigateProps;
+interface TypedNavigatePropsWithParam<P extends RoutesPath> {
+    to: P;
+    params: PathParams<P>;
+    search?: string;
+}
+
+export type NavigatePropsAll<P extends RoutesPath> = P extends `${string}:${string}`
+    ? TypedNavigatePropsWithParam<P> & NavigateProps
+    : TypedNavigateProps<P> & NavigateProps;
