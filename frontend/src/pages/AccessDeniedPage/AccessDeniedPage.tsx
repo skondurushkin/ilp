5f095b6a1351a2c076b5e76cf2cacef46ea84d0f
Nikolay Stupak <nikolaystupak@gmail.com>
2023-03-30 11:23:20 +0300

    refactor: styles of the not found page and the access denied page

--- frontend/src/pages/AccessDeniedPage/AccessDeniedPage.tsx
+++ frontend/src/pages/AccessDeniedPage/AccessDeniedPage.tsx
@@ -7,7 +7,7 @@ export const AccessDeniedPage = () => {
         <Layout>
             <div className="flex min-h-full w-full flex-col items-center justify-center">
                 <img src={gendalfUrl} alt="Гендальф" />
-                <h1 className="mt-6">Нет доступа для просмотра этой страницы</h1>
+                <h1 className="text mt-6">Нет доступа для просмотра этой страницы</h1>
                 <TypedLink className="mt-5" to="/" presentation="button" primary>
                     Вернуться на главную страницу
                 </TypedLink>
--- frontend/src/pages/NotFoundPage/NotFoundPage.tsx
+++ frontend/src/pages/NotFoundPage/NotFoundPage.tsx
@@ -7,7 +7,7 @@ export const NotFoundPage = () => {
         <Layout>
             <div className="flex min-h-full w-full flex-col items-center justify-center">
                 <img src={obivanKenobiUrl} alt="Оби-Ван Кеноби" />
-                <h1 className="mt-6">Это не та страница, которую вы ищете</h1>
+                <h1 className="text mt-6">Это не та страница, которую вы ищете</h1>
                 <TypedLink className="mt-5" to="/" presentation="button" primary>
                     Вернуться на главную страницу
                 </TypedLink>
