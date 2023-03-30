ca76407cb2d04b982408871171263448eb8013db
Nikolay Stupak <nikolaystupak@gmail.com>
2023-03-30 18:11:52 +0300

    feat: add stubs for all pages

--- frontend/src/router/authUserRoutes.tsx
+++ frontend/src/router/authUserRoutes.tsx
@@ -1,13 +1,13 @@
-import { MainPage, NotFoundPage, Profile, RulesPage } from '../pages';
+import { MainPage, NotFoundPage, ProfilePage, RulesPage } from '../pages';
 
+import { Layout } from '../components/Layout';
 import { TypedNavigate } from './components/TypedNavigate';
 
 export const authUserRoutes = [
     { path: '/sign-in', element: <TypedNavigate to="/" replace /> },
     { path: '/', element: <MainPage /> },
-    { path: '/profile', element: <Profile /> },
-    { path: '/admin', element: <div>Admin page</div> },
+    { path: '/profile', element: <ProfilePage /> },
+    { path: '/admin', element: <Layout>Admin page</Layout> },
     { path: '/rules', element: <RulesPage /> },
-    { path: '/support', element: <div>Support page</div> },
     { path: '*', element: <NotFoundPage /> },
 ] as const;
