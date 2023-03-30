136e3c8a100ac096d3d134ad2954f8cab3183fe0
Nikolay Stupak <nikolaystupak@gmail.com>
2023-03-30 10:56:55 +0300

    feat: add rules page

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -1,5 +1,6 @@
 export { SignInPage } from './SignInPage';
 export { MainPage } from './MainPage';
 export { Profile } from './Profile';
+export { RulesPage } from './RulesPage';
 export { NotFoundPage } from './NotFoundPage';
 export { AccessDeniedPage } from './AccessDeniedPage';
