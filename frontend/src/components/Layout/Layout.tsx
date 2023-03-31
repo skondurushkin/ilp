9a201bb70db76db38fa1f01b69dd1b255059e538
Nikolay Stupak <nikolaystupak@gmail.com>
2023-03-31 17:17:46 +0300

    fix: nav width

--- frontend/src/components/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout.tsx
@@ -26,7 +26,7 @@ export function Layout(props: LayoutProps): ReactElement {
         <div className="relative flex min-h-[100vh] w-full flex-col">
             <header className="sticky top-0 z-40 bg-black text-white">
                 <div className="flex justify-between py-4 px-4 sm:px-8 md:px-14 xl:mx-auto xl:max-w-[1280px] xl:py-6 xl:pr-8 xl:pl-0">
-                    <div className="flex items-center xl:min-w-[312px] xl:pl-8">
+                    <div className="flex items-center xl:w-[312px] xl:pl-8">
                         <TypedLink to="/">
                             <Logo />
                         </TypedLink>
@@ -54,7 +54,7 @@ export function Layout(props: LayoutProps): ReactElement {
                     </button>
                 </div>
             </header>
-            <div className="flex grow flex-col bg-white text-black dark:bg-gray-dark dark:text-white">
+            <div className="app-bg flex grow flex-col">
                 <div className="relative flex w-full w-full grow xl:mx-auto xl:max-w-[1280px]">
                     <aside
                         ref={sidebarRef}
@@ -66,7 +66,7 @@ export function Layout(props: LayoutProps): ReactElement {
                             mobileSidebarVisible ? 'translate-x-0' : 'translate-x-full',
                             'fixed top-0 bottom-0 right-0 left-0 z-40 transition-transform',
                             // tablet, screen
-                            'sm:left-auto sm:min-w-[312px]',
+                            'sm:left-auto sm:w-[312px]',
                             // wide screen
                             'xl:top-[92px] xl:right-auto xl:translate-x-0',
                         )}
