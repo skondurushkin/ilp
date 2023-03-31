30fcd52efc813214339f923b05e91c07750260a1
AFilatov <afilatov@it-one.ru>
2023-03-31 10:44:10 +0400

    Добавлен поиск активностей

--- backend/ilp/ilp-persistence/src/main/java/ru/itone/ilp/persistence/repositories/ActivityRepository.java
+++ backend/ilp/ilp-persistence/src/main/java/ru/itone/ilp/persistence/repositories/ActivityRepository.java
@@ -1,9 +1,25 @@
 package ru.itone.ilp.persistence.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.itone.ilp.persistence.entities.Activity;
 
+import java.util.List;
+
 @Repository
 public interface ActivityRepository extends JpaRepository<Activity, Long> {
+
+    String SEARCH_ACTIVITY = """
+            SELECT DISTINCT a.* FROM activities a WHERE
+            a.name ILIKE %:text% OR
+            a.description ILIKE %:text%
+        LIMIT 50
+        """;
+
+    @Query(value = SEARCH_ACTIVITY, nativeQuery = true)
+    List<Activity> searchByText(@Param("text") String text);
+
+
 }
