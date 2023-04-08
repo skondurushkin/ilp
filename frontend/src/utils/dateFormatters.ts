ee88880661392bdf6f7b2b4a20512d6c5b9b842b
Shalovilo <shalovilo33rus@gmail.com>
2023-04-08 14:04:03 +0300

    fix: after review

--- frontend/src/utils/dateFormatters.ts
+++ frontend/src/utils/dateFormatters.ts
@@ -1,39 +1,79 @@
-export const toIsoString = (date: Date): Date => {
-    const tzo = -date.getTimezoneOffset(),
-        dif = tzo >= 0 ? '+' : '-',
-        pad = (num: number) => {
-            return (num < 10 ? '0' : '') + num;
-        };
+import addDays from 'date-fns/addDays';
+import addMonths from 'date-fns/addMonths';
+import addYears from 'date-fns/addYears';
+import toFormat from 'date-fns/format';
 
-    return (date.getFullYear() +
-        '-' +
-        pad(date.getMonth() + 1) +
-        '-' +
-        pad(date.getDate()) +
-        'T' +
-        pad(date.getHours()) +
-        ':' +
-        pad(date.getMinutes()) +
-        ':' +
-        pad(date.getSeconds()) +
-        dif +
-        pad(Math.floor(Math.abs(tzo) / 60)) +
-        ':' +
-        pad(Math.abs(tzo) % 60)) as unknown as Date;
-};
+interface Period {
+    start: Date;
+    end: Date;
+}
 
-export const toISODateString = (date: Date): Date => {
-    const pad = (num: number) => {
-        return (num < 10 ? '0' : '') + num;
-    };
+type Interval = 'hour' | 'day' | 'week' | 'month' | 'year';
 
-    return (date.getFullYear() + '-' + pad(date.getMonth() + 1) + '-' + pad(date.getDate())) as unknown as Date;
-};
+// сдвиг даты, Interval с минусом означает сдвиг в прошлое, без минуса - в будущее
+type DateShift = Interval | `-${Interval}`;
 
-export const getDateMinusDays = (days: number, date: Date | undefined = new Date()) => {
-    return new Date(date.setDate(date.getDate() - days));
-};
+export function createPeriod(date: Date, shift: DateShift, format?: string): Period {
+    const defaultFormat = format || 'YYYY-MM-DDTHH:mm:ss.SSS';
+    const first = toFormat(date, defaultFormat) as unknown as Date;
+    const second = toFormat(shiftDate(date, shift), defaultFormat) as unknown as Date;
+    return shift.startsWith('-') ? { start: second, end: first } : { start: first, end: second };
+}
 
-export const getDateMinusMonths = (months: number, date: Date | undefined = new Date()) => {
-    return new Date(date.setMonth(date.getMonth() - months));
-};
+export function shiftDate(date: Date, shift: DateShift) {
+    const past = shift.startsWith('-');
+    const interval = past ? (shift.substring(1) as Interval) : (shift as Interval);
+    const multiplier = past ? -1 : 1;
+
+    switch (interval) {
+        case 'day':
+            return addDays(date, multiplier * 1);
+        case 'week':
+            return addDays(date, multiplier * 7);
+        case 'month':
+            return addMonths(date, multiplier * 1);
+        case 'year':
+            return addYears(date, multiplier * 12);
+        default:
+            throw new Error(`shifting a date to "${shift}" is not supported yet`);
+    }
+}
+// export const toIsoString = (date: Date): Date => {
+//     const tzo = -date.getTimezoneOffset(),
+//         dif = tzo >= 0 ? '+' : '-',
+//         pad = (num: number) => {
+//             return (num < 10 ? '0' : '') + num;
+//         };
+
+//     return (date.getFullYear() +
+//         '-' +
+//         pad(date.getMonth() + 1) +
+//         '-' +
+//         pad(date.getDate()) +
+//         'T' +
+//         pad(date.getHours()) +
+//         ':' +
+//         pad(date.getMinutes()) +
+//         ':' +
+//         pad(date.getSeconds()) +
+//         dif +
+//         pad(Math.floor(Math.abs(tzo) / 60)) +
+//         ':' +
+//         pad(Math.abs(tzo) % 60)) as unknown as Date;
+// };
+
+// export const toISODateString = (date: Date): Date => {
+//     const pad = (num: number) => {
+//         return (num < 10 ? '0' : '') + num;
+//     };
+
+//     return (date.getFullYear() + '-' + pad(date.getMonth() + 1) + '-' + pad(date.getDate())) as unknown as Date;
+// };
+
+// export const getDateMinusDays = (days: number, date: Date | undefined = new Date()) => {
+//     return new Date(date.setDate(date.getDate() - days));
+// };
+
+// export const getDateMinusMonths = (months: number, date: Date | undefined = new Date()) => {
+//     return new Date(date.setMonth(date.getMonth() - months));
+// };
