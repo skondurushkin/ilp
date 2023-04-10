cb7101d63a6f19c2fb9ca5db216da9a3a4aff6f1
aleksandr.purtov <aleksandr.purtov@dxc.com>
2023-04-10 15:04:36 +0600

    feat(admin): add preview img modal to PhotoInput

--- frontend/src/components/Form/PhotoInput/PhotoInput.tsx
+++ frontend/src/components/Form/PhotoInput/PhotoInput.tsx
@@ -1,13 +1,15 @@
-import { ChangeEvent, MouseEventHandler, ReactNode, useRef } from 'react';
+import { ChangeEvent, ReactNode, useRef, useState } from 'react';
 import { Control, FieldPath, FieldValues, RegisterOptions, useController } from 'react-hook-form';
 import { ErrorMessage, UploadFileScopeEnum, api } from '../../../api';
 import { PHOTO_IMAGE_ACCEPT, validatePhotoFile } from './PhotoInput.utils';
 
+import { Button } from '../../Button';
 import { DEFAULT_API_ERROR_MSG } from '../../../api/constants';
+import { ReactComponent as EditSVG } from '../../../assets/edit.svg';
 import { FormErrorMessage } from '../FormErrorMessage';
+import Modal from '../../Modal';
 import { ReactComponent as PlusSquareSVG } from '../../../assets/plus-square.svg';
 import { Spinner } from '../../Spinner';
-import { ReactComponent as XSquareVG } from '../../../assets/x-square.svg';
 import { toast } from 'react-toastify';
 import { useMutation } from 'react-query';
 
@@ -20,6 +22,8 @@ interface PhotoInputProps<TFieldValues extends FieldValues> {
 }
 
 export const PhotoInput = <TFieldValues extends FieldValues = FieldValues>(props: PhotoInputProps<TFieldValues>) => {
+    const [previewModalIsVisible, setPreviewModalIsVisible] = useState(false);
+
     const { scope, control, name, rules } = props;
 
     const { field, fieldState } = useController({
@@ -68,40 +72,64 @@ export const PhotoInput = <TFieldValues extends FieldValues = FieldValues>(props
                 onChange={uploadPhoto}
             />
             {field.value ? (
-                <InputButton onClick={() => field.onChange(null)}>
-                    <img alt="Товар" src={field.value} className="h-full w-full object-contain" />
-                    <XSquareVG className="fill-error absolute-center h-6 w-6" />
-                </InputButton>
+                <InputContainer>
+                    <button type="button" className="h-full w-full" onClick={() => setPreviewModalIsVisible(true)}>
+                        <img alt="Товар" src={field.value} className="h-full w-full object-contain" />
+                    </button>
+                    <button
+                        type="button"
+                        className="bg-black-transparent-50% absolute right-0 top-0 p-2"
+                        onClick={() => photoInputRef.current?.click()}
+                    >
+                        <EditSVG className="stroke-primary h-6 w-6" />
+                    </button>
+                </InputContainer>
             ) : (
-                <InputButton onClick={() => photoInputRef.current?.click()}>
-                    {uploadPhotoIsLoading ? (
-                        <div className="absolute-center absolute h-6 w-6  ">
-                            <Spinner className="h-6 w-6" />
-                        </div>
-                    ) : (
-                        <PlusSquareSVG className="stroke-primary absolute-center absolute h-6 w-6" />
-                    )}
-                </InputButton>
+                <button type="button" onClick={() => photoInputRef.current?.click()}>
+                    <InputContainer>
+                        {uploadPhotoIsLoading ? (
+                            <div className="absolute-center absolute h-6 w-6  ">
+                                <Spinner className="h-6 w-6" />
+                            </div>
+                        ) : (
+                            <PlusSquareSVG className="stroke-primary absolute-center absolute h-6 w-6" />
+                        )}
+                    </InputContainer>
+                </button>
             )}
             <FormErrorMessage error={fieldState.error} />
+            <Modal
+                id="PhotoInput"
+                size="md"
+                isOpen={previewModalIsVisible}
+                closeModal={() => setPreviewModalIsVisible(false)}
+            >
+                <Modal.Body>
+                    {previewModalIsVisible && (
+                        <div className="flex flex-col gap-6">
+                            <img alt="Товар" src={field.value} className="h-[500px] w-full object-contain" />
+                            <div className="flex gap-4">
+                                <Button type="button" onClick={() => setPreviewModalIsVisible(false)}>
+                                    Закрыть
+                                </Button>
+                            </div>
+                        </div>
+                    )}
+                </Modal.Body>
+            </Modal>
         </div>
     );
 };
 
-interface InputButtonProps {
-    onClick?: MouseEventHandler<HTMLButtonElement>;
+interface InputContainerProps {
     children: ReactNode;
 }
 
-const InputButton = (props: InputButtonProps) => {
-    const { onClick, children } = props;
+const InputContainer = (props: InputContainerProps) => {
+    const { children } = props;
     return (
-        <button
-            type="button"
-            className="relative h-[110px] w-[110px] border-2 border-dotted border-black bg-white dark:border-white dark:bg-black"
-            onClick={onClick}
-        >
+        <div className="relative h-[110px] w-[110px] border-2 border-dotted border-black bg-white dark:border-white dark:bg-black">
             {children}
-        </button>
+        </div>
     );
 };
