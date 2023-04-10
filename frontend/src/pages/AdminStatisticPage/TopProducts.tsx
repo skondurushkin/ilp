4d7681e70b1532d4f5391b1dd2e9b6f774fd54e0
Shalovilo <shalovilo33rus@gmail.com>
2023-04-10 20:25:20 +0300

    feat: return sort in top statistic

--- frontend/src/pages/AdminStatisticPage/TopProducts.tsx
+++ frontend/src/pages/AdminStatisticPage/TopProducts.tsx
@@ -1,20 +1,18 @@
 import { ArticleResponse, PageRequest, api } from '../../api';
-import { useCallback, useMemo, useState } from 'react';
+import { useCallback, useMemo } from 'react';
 
 import { ARTICLES_STATISTIC_ADMIN_QUERY_KEY } from '../../modules/admin';
 import { AdminTable } from '../../components/AdminTable';
 import type { ColumnDef } from '@tanstack/table-core';
-import { twMerge } from 'tailwind-merge';
 
 export const TopProducts = ({ period }: { period: 'day' | 'all' }) => {
-    const [sort, setSort] = useState<'DESC' | 'ASC'>('DESC');
     const queryData = useCallback(
         (pageRequest: PageRequest) => {
             return api.admin.browseStatisticArticles({
-                browseStatisticArticlesRequest: { ...pageRequest, period, sort },
+                browseStatisticArticlesRequest: { ...pageRequest, period, sort: 'DESC' },
             });
         },
-        [sort, period],
+        [period],
     );
 
     const columns = useMemo<ColumnDef<ArticleResponse>[]>(
@@ -23,7 +21,6 @@ export const TopProducts = ({ period }: { period: 'day' | 'all' }) => {
                 accessorKey: 'id',
                 header: () => <span>ун</span>,
                 cell: (info) => info.getValue(),
-                enableSorting: false,
             },
             {
                 accessorKey: 'name',
@@ -37,63 +34,30 @@ export const TopProducts = ({ period }: { period: 'day' | 'all' }) => {
                         </div>
                     );
                 },
-                enableSorting: false,
             },
             {
                 accessorKey: 'code',
                 header: () => <span>артикул</span>,
                 cell: (info) => info.getValue(),
-                enableSorting: false,
             },
             {
                 accessorKey: 'price',
                 header: () => <span>стоимость</span>,
                 cell: (info) => info.getValue(),
-                enableSorting: false,
             },
             {
                 accessorKey: 'count',
-                enableSorting: false,
-                header: () => (
-                    <button
-                        onClick={() => {
-                            setSort(sort == 'ASC' ? 'DESC' : 'ASC');
-                        }}
-                        className="cursor-pointer border-none bg-none p-0 outline-none"
-                    >
-                        <span className="flex">
-                            Заказы
-                            <svg
-                                width="24"
-                                height="24"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                xmlns="http://www.w3.org/2000/svg"
-                                className={twMerge(
-                                    'stroke-primary h-auto w-4 transform',
-                                    sort === 'ASC' ? 'rotate-90' : '-rotate-90',
-                                )}
-                            >
-                                <path
-                                    d="M15 18L9 12L15 6"
-                                    strokeWidth="2"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                ></path>
-                            </svg>
-                        </span>
-                    </button>
-                ),
+                header: () => <span className="flex">Заказы</span>,
                 cell: (info) => info.getValue(),
             },
         ],
-        [sort],
+        [],
     );
 
     return (
         <div className="py-4">
             <AdminTable
-                queryKey={ARTICLES_STATISTIC_ADMIN_QUERY_KEY + sort + period}
+                queryKey={ARTICLES_STATISTIC_ADMIN_QUERY_KEY + period}
                 globalFilterPlaceholder="Поиск по Наименованию, Артиклу и Стоимости"
                 columns={columns}
                 queryData={queryData}
